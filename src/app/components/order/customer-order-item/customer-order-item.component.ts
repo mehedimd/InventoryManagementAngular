0825f65c57685554ec67a47b301ae25abfe2641e
mehedi <mehedi.it@labaidpharma.com>
2025-07-23 16:50:54 +0600

    stock alert added

--- src/app/components/order/customer-order-item/customer-order-item.component.ts
+++ src/app/components/order/customer-order-item/customer-order-item.component.ts
@@ -10,6 +10,7 @@ import {
 } from '@angular/forms';
 import { MatDialogClose } from '@angular/material/dialog';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-customer-order-item',
@@ -67,7 +68,8 @@ export class CustomerOrderItemComponent implements OnInit{
     if (e.target.selectedIndex != 0) {
       this.orderItemForm.patchValue({
         unitPrice: this.allProducts[e.target.selectedIndex - 1].unitPrice,
-        productName: this.allProducts[e.target.selectedIndex - 1].productName
+        productName: this.allProducts[e.target.selectedIndex - 1].productName,
+        reorderLevel : this.allProducts[e.target.selectedIndex - 1].reorderLevel
       });
       this.stockQuantity = this.allProducts[e.target.selectedIndex - 1].stockQuantity;
       // update total
@@ -92,12 +94,44 @@ export class CustomerOrderItemComponent implements OnInit{
 
   // add order item
   addOrderItem() {
-    if (this.data.itemIndex == null) {
-      this.orderService.orderItems.push(this.orderItemForm.value);
-      this.dialogRef.close();
+
+    if (this.orderItemForm.value.quantity > this.stockQuantity) {
+      Swal.fire({
+        icon: 'error',
+        title: '❌ Insufficient Stock',
+        text: `Not enough stock available for this product. Only ${this.stockQuantity} in stock.`,
+        confirmButtonText: 'OK',
+      });
+      return;
+    }
+
+    if (this.stockQuantity < this.orderItemForm.value.reorderLevel) {
+      Swal.fire({
+        icon: 'warning',
+        title: '⚠️ Low Stock Warning',
+        text: 'This product has reached its reorder level. Do you want to proceed?',
+        showCancelButton: true,
+        confirmButtonText: 'Proceed',
+        cancelButtonText: 'Cancel',
+      }).then((result) => {
+        if (result.isConfirmed) {
+ 
+          if (this.data.itemIndex == null) {
+            this.orderService.orderItems.push(this.orderItemForm.value);
+          } else {
+            this.orderService.orderItems[this.data.itemIndex] = this.orderItemForm.value;
+          }
+          this.dialogRef.close();
+        }
+
+      });
     } else {
-      this.orderService.orderItems[this.data.itemIndex] =
-        this.orderItemForm.value;
+
+      if (this.data.itemIndex == null) {
+        this.orderService.orderItems.push(this.orderItemForm.value);
+      } else {
+        this.orderService.orderItems[this.data.itemIndex] = this.orderItemForm.value;
+      }
       this.dialogRef.close();
     }
   }
@@ -110,7 +144,8 @@ export class CustomerOrderItemComponent implements OnInit{
     unitPrice: [0],
     quantity: [0, [Validators.required, Validators.min(1)]],
     subTotal: [0],
-    stockQuantity: [0]
+    stockQuantity: [0],
+    reorderLevel: [0]
   });
   }
 }
